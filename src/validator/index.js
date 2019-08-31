138abd3f7d312ce1cd2e015bc8070c97ec11c120
JacobKnaack <jacobknck@gmail.com>
2019-08-30 18:15:38 -0700

    adds minor updates

---
+++ src/validator/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const diff = require('jsdiff');
+
+/**
+ *
+ * @type {{}}
+ */
+
+class Validator {
+  constructor () {
+    this.source = null;
+    this.test = null;
+  }
+
+  traverse() {
+
+  }
+
+}
+
+module.exports = Validator;
